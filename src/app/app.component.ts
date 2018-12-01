539e13cde3479d67ded6e3ec9d53d58b020d010c
melnele <mohamed.nele@gmail.com>
2018-12-01 15:53:19 +0200

    frontend almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GUC-Compiler';
+  code = '';
+  stdin = '';
+  stdout = '';
+  static codefile = '';
+
+  constructor(public http: HttpClient) { }
+
+  static getfiletext(str) {
+    this.codefile = str;
+  }
 }
